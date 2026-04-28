55bc94ffcb35ac092d6f28d01cb6e90d5052e153
Massimo Bonanni <massimo.bonanni@tiscali.it>
2026-04-28 14:17:49 +0200

    Add cryptography functionality to Program.cs

--- MyAmazingConsole/Program.cs
+++ MyAmazingConsole/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
+des.GenerateKey();
+des.GenerateIV();
