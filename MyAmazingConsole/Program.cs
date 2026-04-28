9006a7c63ea2d67bcffb2ff2b8b9fb9b8123a09a
Massimo Bonanni <massimo.bonanni@tiscali.it>
2026-04-28 14:29:56 +0200

    Remove weak cryptography implementation from Program.cs

--- MyAmazingConsole/Program.cs
+++ MyAmazingConsole/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Security.Cryptography;
+﻿
 
 Console.WriteLine("Hello, World!");
 
-var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
-des.GenerateKey();
-des.GenerateIV();
