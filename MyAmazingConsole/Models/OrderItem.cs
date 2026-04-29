bf11ad124b756068e77beae84eb02f8470dcfd81
Massimo Bonanni <massimo.bonanni@tiscali.it>
2026-04-29 14:10:55 +0200

    Fix constructor assignment in OrderItem class to set Code correctly

--- MyAmazingConsole/Models/OrderItem.cs
+++ MyAmazingConsole/Models/OrderItem.cs
@@ -44,7 +44,7 @@ public class OrderItem
     /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="quantity"/> is less than or equal to zero.</exception>
     public OrderItem(string code, string description, int quantity, decimal unitCost)
     {
-        Code = description;
+        Code = code;
         Description = description;
         Quantity = quantity;
         UnitCost = unitCost;
